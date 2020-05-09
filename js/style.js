b2afcda5be6719a8590e8bf9094802282b86c3d1
Sanchit Pandey <sanchitpandey72@gmail.com>
2020-05-09 14:52:42 +0530

    Added search functionality and centered background

--- js/style.js
+++ js/style.js
@@ -5,19 +5,28 @@ function time() {
   //timeNow.textContent = h + ":" + m + " "+ format;
 }
 
+var searchBox = document.getElementById('searchInput');
+
+searchBox.addEventListener('keyup', (e) => {
+  if (e.key == "Enter"){
+    console.log(e.target.value);
+    window.open("https://www.google.com/search?q="+e.target.value, "_blank");
+  }
+});
+
 setInterval(time, 1000);
 
-	var myDate = new Date();
-  	if (myDate.getHours() < 12) {
-        document.getElementById('greeting').innerHTML="Good Morning ";
-    }
-    else if(myDate.getHours() >=12 && myDate.getHours() <=17){
-        document.getElementById('greeting').innerHTML="Good Afternoon ";
-    }
-    else if (myDate.getHours() > 17 && myDate.getHours() <=24) {
-        document.getElementById('greeting').innerHTML="Good Evening ";
-    }
-    else
-    {
-        document.getElementById('greeting').innerHTML="Good Night ";
-    }
+var myDate = new Date();
+if (myDate.getHours() < 12) {
+    document.getElementById('greeting').innerHTML="Good Morning ";
+}
+else if(myDate.getHours() >=12 && myDate.getHours() <=17){
+    document.getElementById('greeting').innerHTML="Good Afternoon ";
+}
+else if (myDate.getHours() > 17 && myDate.getHours() <=24) {
+    document.getElementById('greeting').innerHTML="Good Evening ";
+}
+else
+{
+    document.getElementById('greeting').innerHTML="Good Night ";
+}
